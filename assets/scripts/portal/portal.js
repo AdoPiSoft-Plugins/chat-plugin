b29ec95e30f9d2703c36a11e61fe34531d109c02
Arnel T. Lenteria <arnel.lenteria@gmail.com>
2020-04-10 20:05:44 +0800

    cleanup

--- assets/scripts/portal/portal.js
+++ assets/scripts/portal/portal.js
@@ -54,7 +54,7 @@ function disconnected(){
   li.id = 'disconnected'
   li.style.textAlign = 'center';
   li.style.padding = "20px";
-  li.innerHTML = "<a href='javascript:window.location.reload()'>Something went wrong, please reload this page</a>";
+  li.innerHTML = "<a href='javascript:window.location.reload()'>Something went wrong? Click here to reload the page</a>";
   ul.append(li)
   scrollToBottom()
 }
@@ -355,7 +355,7 @@ function promptAppInstallation(){
   li.id = 'message-0'
   var innerHTML = '<div class="message received">'
   innerHTML = innerHTML + '<strong class="sender">Admin</strong><br/>'
-  innerHTML = innerHTML + '<pre class="text" style="padding-bottom: 0;margin-bottom: 0;">Install CaptivePortal App to receive instant notifications. Please ignore this message if you already installed it.</pre>'
+  innerHTML = innerHTML + '<pre class="text" style="padding-bottom: 0;margin-bottom: 0;">We have an Android App for you to conveniently open the captive portal and to receive notifications. Click the download link below to install it. Disregard this message if you already installed it.</pre>'
   innerHTML = innerHTML + '<p style=" text-align: center; "><a style=" background: #209e91!important; color: white !important; padding: 10px; border-radius: 10px; display: inline-block; width: 210px; text-align: center; margin:10px;" href="/plugins/chat-plugin/assets/captive-portal.apk">Download App</a></p>';
   innerHTML = innerHTML + '<small class="time">' + formatDate(new Date()) + '</small></div>'
   li.innerHTML = innerHTML
